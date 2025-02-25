d97a679c449c14ab5517127a8cdc584983d0cd1a
sahaupasana <sahaupasana2003@gmail.com>
2025-02-25 18:06:34 +0000

    data config done

---
+++ basics/datatype.js
@@ -0,0 +1,16 @@
+"use strict" ; //for using the latest version of the javascript which is by default 
+// alert (3+3);   not possible in node js in browser only 
+
+console.log(3
+    +3);//readability is poor and has to be maintained always 
+//datatypes in javascript 
+//integer , string , boolean , 
+//undefined - variable in declared but value is not assigned 
+//null- empty value is assigned (standalone value)
+//symbol for uiquemess 
+
+
+//non primitive - object 
+console.log(typeof(null)); //result will  be object 
+console.log(typeof(undefined));//result will be undefined 
+
---
+++ basics/variables.js
@@ -0,0 +1,11 @@
+const accountId=1234 // cant be changed 
+let accountemail="sahaupasana@"
+var accountpass="12345"
+accountcity="abc"
+
+
+accountemail="agjhg@"
+accountpass="65432"
+accountcity="pou"
+
+console.table([accountId,accountemail,accountpass,accountcity])
