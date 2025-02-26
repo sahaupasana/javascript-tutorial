e2f79f23862282dbd07b0a7fb35cf884fd041209
sahaupasana <sahaupasana2003@gmail.com>
2025-02-26 17:34:12 +0000

    comparisons dompleted

---
+++ basics/comparison.js
@@ -0,0 +1,14 @@
+//values of comparisons come in boolean 
+// comparing different data types 
+ console.log("2">1)
+ console.log("01">1)//not reliable results (shouls be of same data type )
+
+//conversion >= converts null to 0 
+console.log(null==0);
+console.log(null>=0);
+
+//strict check === checks data types 
+console.log("2"==2);
+console.log("2"===2)
+
+ 
---
+++ basics/operation.js
@@ -0,0 +1,18 @@
+let value =3
+let negValue= -value
+console.log(value);
+console.log(negValue);
+
+let str1= "Upasana"
+let str2=" Saha"
+console.log(str1+str2); // concatenation
+
+// when we try to concatenate a string and an integer together 
+console.log(1+"2");
+console.log(1+2+"3")
+console.log("1"+2+3); // both would give different results 
+
+console.log(+true) // tricky conversion to integer 
+
+
+
